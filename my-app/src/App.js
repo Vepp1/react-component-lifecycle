5ee28a43470a38f6ad9fb2d01f59aa78115c37c5
Vepp1 <vinipontarolla@gmail.com>
2022-12-06 22:04:35 +0000

    Component Did Mount

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LifeCyclesCDM from './components/LifeCyclesCDM';
+
+function App() {
+  return (
+    <div className="App">
+      <LifeCyclesCDM />
+    </div>
+  );
+}
+
+export default App;
