a02aecc84b96bdc904d205366aa7cc4e6dce8fa5
Vepp1 <vinipontarolla@gmail.com>
2022-12-07 10:30:00 +0000

    Component Did Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import LifeCyclesCDM from './components/LifeCyclesCDM';
+import LifeCyclesCDU from './components/LifeCyclesCDU';
 
 function App() {
   return (
     <div className="App">
-      <LifeCyclesCDM />
+      <LifeCyclesCDU />
     </div>
   );
 }
